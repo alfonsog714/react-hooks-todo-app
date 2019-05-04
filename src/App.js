f90f1b4b39214e0362284a7bf2781cbe9160686c
Alfonso <alfonso.garcia2324@gmail.com>
2019-05-04 14:30:45 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about react hooks",
+      isCompleted: false
+    },
+    {
+      text: "Pickup paycheck",
+      isCompleted: false
+    },
+    {
+      text: "Build todo app",
+      isCompleted: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
